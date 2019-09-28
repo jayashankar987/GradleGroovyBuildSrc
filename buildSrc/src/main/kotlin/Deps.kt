aa96e8548ccadc89e8b24ea8b6b3ff4c425fe5fc
Jayashankar Boya <jayashankar987@gmail.com>
2019-09-28 11:46:58 +0530

    initial commit

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,13 @@
+
+object Versions {
+    const val androidx_version = "1.1.0"
+    const val kotlin_version = "1.3.41"
+}
+
+
+
+object Deps {
+    val androidx = object {
+         val appcompat = "androidx.appcompat:appcompat:${Versions.androidx_version}"
+    }
+}
